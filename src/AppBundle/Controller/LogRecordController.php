4e3ad4485edb8ac706dbdbad71d3db80c2df4571
Владимир Фалушко <vladimir.falushko@jelvix.com>
2016-08-16 16:00:05 +0300

    LogRecord entity and controller added. Doctrine mapping and entity validation added.

---
+++ src/AppBundle/Controller/LogRecordController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\HttpFoundation\Request;
+
+class LogRecordController extends FOSRestController
+{
+    public function getLogRecordsAction(Request $request)
+    {
+
+    }
+}
--- src/AppBundle/Entity/LogRecord.php
+++ src/AppBundle/Entity/LogRecord.php
@@ -4,5 +4,7 @@ namespace AppBundle\Entity;
 
 class LogRecord
 {
-
+    private $id;
+    private $datetime;
+    private $record;
 }
