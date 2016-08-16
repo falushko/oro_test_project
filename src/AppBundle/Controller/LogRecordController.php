21df753e0c40028bf314334453a74eb74ed39870
Владимир Фалушко <vladimir.falushko@jelvix.com>
2016-08-16 16:05:29 +0300

    Validation trait added.

--- src/AppBundle/Controller/LogRecordController.php
+++ src/AppBundle/Controller/LogRecordController.php
@@ -5,6 +5,10 @@ namespace AppBundle\Controller;
 use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * Class LogRecordController
+ * @package AppBundle\Controller
+ */
 class LogRecordController extends FOSRestController
 {
     public function getLogRecordsAction(Request $request)
--- src/AppBundle/Entity/LogRecord.php
+++ src/AppBundle/Entity/LogRecord.php
@@ -2,8 +2,12 @@
 
 namespace AppBundle\Entity;
 
+use AppBundle\Traits\ValidationTrait;
+
 class LogRecord
 {
+    use ValidationTrait;
+
     private $id;
     private $datetime;
     private $record;
